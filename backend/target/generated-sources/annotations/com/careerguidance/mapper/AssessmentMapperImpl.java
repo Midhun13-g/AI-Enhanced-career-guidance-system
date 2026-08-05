d227b0918825b7cbfb2039ed8a678ee7678d124e
Kabilan <kabilan4549@gmail.com>
2026-08-05 09:06:02 +0530

    Updating UI

--- backend/target/generated-sources/annotations/com/careerguidance/mapper/AssessmentMapperImpl.java
+++ backend/target/generated-sources/annotations/com/careerguidance/mapper/AssessmentMapperImpl.java
@@ -19,8 +19,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-30T09:33:38+0530",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
+    date = "2026-08-05T08:32:46+0530",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.46.100.v20260624-0231, environment: Java 21.0.11 (Eclipse Adoptium)"
 )
 @Component
 public class AssessmentMapperImpl implements AssessmentMapper {
